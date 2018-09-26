ad6b5b27ea002b7a4171e108aee0ce26414804f5
Luke Lee <durdenmisc@gmail.com>
2018-09-26 18:59:40 +0200

    Feature Request 1: Print lines in files in directory

--- wc.py
+++ wc.py
@@ -2,11 +2,28 @@
 Script to mostly mimic the `wc` command in Unix on all platforms
 """
 
+import os
 import sys
 
-with open(sys.argv[1], 'r', encoding='utf-8') as file_obj:
-    count = 0
-    for _ in file_obj:
-        count += 1
 
-print(count)
+def count_lines(file_):
+    with open(file_, 'r', encoding='utf-8') as file_obj:
+        count = 0
+        for _ in file_obj:
+            count += 1
+
+    return count
+
+
+arg = sys.argv[1]
+
+if os.path.isfile(arg):
+    print(count_lines(arg))
+else:
+    for item in os.listdir(arg):
+        file_ = os.path.join(arg, item)
+        if not os.path.isfile(file_):
+            continue
+
+        num_lines = count_lines(file_)
+        print(f'{file_}: {num_lines}')
